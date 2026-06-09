f3b462b2172ba1e40b4b0dad0070e0f6dfa3974b
ALidym <alidym@ALidymdeMac-mini.local>
2026-06-09 09:39:08 +0800

    fix: iframe添加时间戳防缓存+验证按钮/坐标已更新

--- src/sim-rare.js
+++ src/sim-rare.js
@@ -48,7 +48,7 @@ export function openSimRare(bKey) {
       </div>
       <!-- Iframe body -->
       <div class="sr-body" id="sr-body" style="display:none">
-        <iframe class="sr-iframe" src="/rare/index.html?project=${bKey}" loading="eager"></iframe>
+        <iframe class="sr-iframe" src="/rare/index.html?project=${bKey}&_t=${Date.now()}" loading="eager"></iframe>
       </div>
     </div>
   `
